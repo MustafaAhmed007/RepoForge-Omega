c81436d6782efd6418dea7f621e85a883f968667
Mustafa Ahmed <150585723+MustafaAhmed007@users.noreply.github.com>
2026-09-23 16:57:36 +0530

    feat: add model routing and deployment goal API

---
+++ repoforge/goal.py
@@ -0,0 +1,3 @@
+from .goals import DEPLOYMENT_READY_GOAL, Goal, GoalRunner, build_deployment_task_graph, deployment_goal
+
+__all__ = ["DEPLOYMENT_READY_GOAL", "Goal", "GoalRunner", "build_deployment_task_graph", "deployment_goal"]
